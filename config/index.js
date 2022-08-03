31c21dfd56188cd3faaf83b1b11e90e40a9fe978
Datallboy <github@datallboy.com>
2022-08-03 11:40:00 -0400

    Backup

--- config/index.js
+++ config/index.js
@@ -5,4 +5,12 @@ exports.GUILD_ID = '455345206481518593';
 exports.STAFF_ROLES = ['Staff', 'Server Admin'];
 
 exports.BOT_TOKEN = process.env.FOXY_BOT_TOKEN;
-exports.TH_API_URL = process.env.TH_API_URL || 'http://localhost:5000/api/v1';
+
+exports.PROXMOX_URL =
+  process.env.PROXMOX_URL || 'https://lab.techhaven.io/api2/json';
+exports.PROXMOX_TOKEN = process.env.PROXMOX_TOKEN;
+
+exports.PROXMOX_VM_IDS = {
+  sift: 9005,
+  flareVm: 9006,
+};
